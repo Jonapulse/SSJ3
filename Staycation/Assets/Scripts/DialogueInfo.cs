fc4a2986c4539e4be45e9138919474b706a3973c
Jonah Pulsipher <jonapulse@gmail.com>
2021-08-28 13:36:19 -0600

    Adding dialogue input system

---
+++ Staycation/Assets/Scripts/DialogueInfo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueInfo : MonoBehaviour {
+
+    public singleDialogue[] dialogueList;
+
+    [System.Serializable]
+    public class singleDialogue
+    {
+        public enum neighborID { Penny, Shae, Gabe, Ezra, radio, groupChat };
+        public neighborID chatOwner;
+        public int chatOrder;
+        public chatInformation[] dialogue;
+
+        [System.Serializable]
+        public class chatInformation
+        {
+            public neighborID whoIsSpeaking;
+            public string whatIsSaid;
+        }
+    }
+    
+
+	
+}
