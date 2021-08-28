ed0a7e732a99d7664cc646c99e13c59dc4a58d78
Jonah Pulsipher <jonapulse@gmail.com>
2021-08-28 14:01:22 -0600

    Dialogue tweaks and dummy conversation

--- Staycation/Assets/Scripts/DialogueInfo.cs
+++ Staycation/Assets/Scripts/DialogueInfo.cs
@@ -8,7 +8,8 @@ public class DialogueInfo : MonoBehaviour {
     [System.Serializable]
     public class singleDialogue
     {
-        public enum neighborID { Penny, Shae, Gabe, Ezra, radio, groupChat };
+        public string name;
+        public enum neighborID { Penny, Shae, Gabe, Ezra, radio, groupChat, player };
         public neighborID chatOwner;
         public int chatOrder;
         public chatInformation[] dialogue;
